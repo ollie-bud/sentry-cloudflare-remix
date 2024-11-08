93fab8a57376f5155ab23a21e1022a4f71f35d4e
Ollie W <ollie.williams@thisisbud.com>
2024-11-08 09:40:56 +0000

    chore: followed https://docs.sentry.io/platforms/javascript/guides/cloudflare/frameworks/remix/ instructions

---
+++ functions/_middleware.js
@@ -0,0 +1,11 @@
+import * as Sentry from "@sentry/cloudflare";
+
+export const onRequest = [
+  // Make sure Sentry is the first middleware
+  Sentry.sentryPagesPlugin((context) => ({
+    dsn: "https://2348aa2b901bb45ea74a6da3af267a35@o4505885946478592.ingest.us.sentry.io/4508261956911104",
+    // Set tracesSampleRate to 1.0 to capture 100% of spans for tracing.
+    tracesSampleRate: 1.0,
+  })),
+  // Add more middlewares here
+];
